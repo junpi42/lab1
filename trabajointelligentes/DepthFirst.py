2cbf1847ed98facc6c2bde8021112b2f73edcd62
Alvaro <alvaronavaqui@gmail.com>
2024-10-23 16:45:24 +0200

    Formatted, includes inheritance

---
+++ trabajointelligentes/DepthFirst.py
@@ -0,0 +1,18 @@
+from trabajointelligentes.Search import Search
+
+
+class DepthFirst(Search):
+    def __init__(self, problem):
+        Search.__init__(self, problem)
+        self.openNodes = []
+
+    def insert_node(self, new_node, node_list):
+        node_list.append(new_node)
+
+    def extract_node(self, node_list):
+        return node_list.pop()
+
+    def is_empty(self, node_list):
+        if not self.open_list:
+            return False
+        return True
--- trabajointelligentes/clases.py
+++ trabajointelligentes/clases.py
@@ -1,17 +1,28 @@
-
-
-
 class Action:
-    def __init__(self,origin,destination,distance,velocity):
-        self.origin=origin
-        self.destination=destination
-        self.distance=distance
-        self.velocity=velocity
+    def __init__(self, origin, destination, distance, velocity):
+        self.origin = origin
+        self.destination = destination
+        self.distance = distance
+        self.velocity = velocity
+
+    def __eq__(self, other):
+        if isinstance(other, Action):
+            return (self.origin == other.origin and self.destination == other.destination
+                    and self.distance == other.distance)
+        return False
+
+    def __repr__(self):
+        return f"Action({self.origin}, {self.destination}, {self.distance})"
+
+    def __str__(self):
+        return f"\nOrigin:  + {self.origin} + \nDestiny:  + {self.destination} + \nCost:  + {self.distance}"
+
 
 class State:
     """
     Esta clase guarda la posición de nuestro nodo en latitud y longitud
     """
+
     def __init__(self, latitude, longitude, identificador):
         self.latitude = latitude
         self.longitude = longitude
@@ -19,16 +30,30 @@ class State:
 
     def __str__(self):
         return f"State(id: {self.identificador}, lat: {self.latitude}, lon: {self.longitude})"
-        
+
     def __repr__(self):
-        return self.__str__() 
+        return self.__str__()
+
+
 class Node:
     """
-    esta es una clase padre que contiene las otras 2
+    Esta es una clase padre que contiene las otras 2
     """
-    def __init__(self,estado,action,parent=None):
-        self.estado=estado
-        self.action=action
-        self.parent=parent
-        
+
+    def __init__(self, estado, action, parent=None):
+        self.estado = estado
+        self.action = action
+        self.parent = parent
+
+    def __eq__(self, id2):
+        if isinstance(id2, Node):
+            return self.estado.__eq__(id2.estado) and self.parent.__eq__(id2.parent) and self.action.__eq__(
+                id2.action)
+        return False
+
+    def __repr__(self):
+        return f"Action({self.estado}, {self.parent}, {self.action})"
+
+    def __str__(self):
+        return f"\nOrigin:  + {self.action} + \nDestiny:  + {self.parent} + \nCost:  + {self.action}"
 
